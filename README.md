aa68c5aadff36fadd2c13dd062728c67566dff00
Fredrik Gundersen <fgun@ecdc.no>
2021-06-29 12:02:46 +0200

    v 0.2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tmux
+
+Run curl https://raw.githubusercontent.com/iLoop2/tmux/main/install.sh | sh
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-curl 
+curl https://raw.githubusercontent.com/iLoop2/tmux/main/.tmux.conf -o ~/.tmux.conf
+
