98f0073092bc8b5a3be788d66917dd16c623ee50
Fredrik Gundersen <iLoop2@users.noreply.github.com>
2021-08-03 12:45:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 curl https://raw.githubusercontent.com/iLoop2/tmux/main/.tmux.conf -o ~/.tmux.conf
+tmux new-session -d -s myS -n myW
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+tmux kill-server
 
