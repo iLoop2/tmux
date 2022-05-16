2dcdd123f02617ca014cb8bc067f6bd46b9ef40d
Fredrik Gundersen <iLoop2@users.noreply.github.com>
2022-05-16 15:07:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 curl https://raw.githubusercontent.com/iLoop2/tmux/main/.tmux.conf -o ~/.tmux.conf
 export TMUX_PLUGIN_MANAGER_PATH='~/.tmux/plugins/'
  ~/.tmux/plugins/tpm/bin/install_plugins
+
+sudo apt install xsel
