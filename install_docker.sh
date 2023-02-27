1bee3f440a6559f35bd3f9c9718b569b978168cf
Fredrik Gundersen <iLoop2@users.noreply.github.com>
2023-02-27 11:12:08 +0100

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -10,6 +10,7 @@ sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-sudo wget https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64 -O /usr/local/bin/docker-compose
+#sudo wget https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64 -O /usr/local/bin/docker-compose
 
-sudo chmod 755 /usr/local/bin/docker-compose
+#sudo chmod 755 /usr/local/bin/docker-compose
+sudo apt install docker-compose -y
