200a2a2aa350bf68c250599de99f221f68be06b5
Fredrik Gundersen <fredrik.gundersen@mac.com>
2024-03-15 19:23:03 +0100

    Add option to have ctrl-b as prefix

--- install.sh
+++ install.sh
@@ -5,3 +5,13 @@ export TMUX_PLUGIN_MANAGER_PATH='~/.tmux/plugins/'
  ~/.tmux/plugins/tpm/bin/install_plugins
 
 sudo apt install xsel
+
+while true; do
+    read -p "Do you like to change to ctrl-b as prefix (y/n) ? " yn
+    case $yn in
+        [Yy]* ) sed -i -e 's/C-a/C-b/g' ~/.tmux.conf; 
+        break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
