28a9efc4033b8bd5292ed03ef1d8812b1a6e14a0
Fredrik Gundersen <iLoop2@users.noreply.github.com>
2025-05-21 19:59:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+sudo apt install tmux
+sudo apt install git
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 curl https://raw.githubusercontent.com/iLoop2/tmux/main/.tmux.conf -o ~/.tmux.conf
